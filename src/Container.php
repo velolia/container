74d6b78b6a9106f0bebc61396bc95422bea8a04e
Velolia <veloliafw@gmail.com>
2025-05-12 15:46:44 +0700

    Update Container.php
    Fix static instance

--- src/Container.php
+++ src/Container.php
@@ -66,11 +66,7 @@ class Container implements ContainerInterface
     */
     public static function getInstance(): static
     {
-        if (is_null(static::$instance)) {
-            static::$instance = new static();
-        }
-
-        return static::$instance;
+        return static::$instance ??= new static();
     }
 
     /**
